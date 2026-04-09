bd7eaedbd0331f3f3cdaa1e2cabf35ddbe466022
Leslie John <44293311+boogsiee@users.noreply.github.com>
2026-04-09 15:58:22 +0800

    server

--- brgyis-api/server.js
+++ brgyis-api/server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const SECRET_KEY = process.env.JWT_SECRET || "fallback_secret";
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 
 const DB_PATH = path.resolve(__dirname, 'is.db');
--- brgyis/src/api.js
+++ brgyis/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // This logic handles the switch for you automatically
 const API_BASE_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000'  // Your new local Backend port
-  : 'https://your-backend-name.up.railway.app'; // Your Railway URL
+  : 'barangayhub-production.up.railway.app'; // Your Railway URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
